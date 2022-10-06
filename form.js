b84e03afcddc143c9ca0551773e2c9cd4886f307
Frecuero <96363833+Frecuero@users.noreply.github.com>
2022-10-06 16:07:00 -0500

    S

--- form.js
+++ form.js
@@ -1,80 +1,81 @@
 var total = ''
 var punto = false
 var operacion = []
-var saveOp = []
 var op = ''
+var aux = ''
+var auxtotal = ''
 
 num = ((valor)=> {
     punto = total.indexOf(".") == -1? true : false 
     if(((valor != ".") || punto) && (valor != "off" && valor != "ac")){
         total += valor
         operation(valor)
-    }else if(valor == "off" || valor == "ac"){
-        cleanResult()
-    }
+    }else{
+        if(valor == "off" || valor == "ac"){
+            cleanResult()
+        }
+    }    
 })
 
 function operation (valor){
     if(valor == '/' || valor == '*' || valor == '+' || valor == '-' || valor == '='){
         total = total.substring(0, total.length - 1)
-        console.log('valor de operacion total '+ total)
+        console.log('valor de operacion total '+ total + " op "+valor) 
         operacion.push(total)
         switch (valor) {
             case '/':
-                op = '/'
-                total = ''
-                operacion.push(valor)
-                resultado()
+                aux = total
+                total = '0'
+                op='/'
                 break;
             case '*':
-                op = '*'
-                total = ''
-                operacion.push(valor)
-                resultado()
+                aux = total
+                op='*'
                 break;
             case '+':
-                op = '+'
-                total = ''
+                aux = total
+                op='+'
+                total = '0'
                 break;
             case '-':
-                total = ''
+                aux = total
                 op = '-'
                 break;
             case '=':
-                total = ''
                 op="="
-                resultado()
+                total = auxtotal
+                console.log('Total '+total)
                 break;
             }
+    }else {
+        if(op != ''){
+            console.log("valor op "+ op)
+            switch (op) {
+                case '+':
+                    auxtotal = (parseFloat(aux) + parseFloat(valor)).toString()
+                    op=''
+                    total = ''
+                    break;
+                case '-':
+                    auxtotal = (parseFloat(aux) - parseFloat(valor)).toString()
+                    op=''
+                    total = ''
+                    break;
+                case '*':
+                    auxtotal = (parseFloat(aux) * parseFloat(valor)).toString()
+                    op=''
+                    total = ''
+                    break;
+                case '/':
+                    auxtotal = (parseFloat(aux) / parseFloat(valor)).toString()
+                    op=''
+                    total = ''
+                    break;
+            }
+            console.log('entro a op '+ auxtotal)
+        }
     }
     $("#result").val(total)
-}
-
-function resultado(){ 
-
-    if (operacion.length > 3){
-        for (let i = 0; i < operacion.length; i++) {
-            if(operacion[i-1]== "+"){
-                a = operacion[i-2]
-                b = operacion[i]
-                r = parseFloat(a) + parseFloat(b)
-                operacion = []
-                operacion.push(r)
-            }    
-            if(operacion[i-1]== "-"){
-                a = operacion[i-2]
-                b = operacion[i]
-                r = parseFloat(a) - parseFloat(b)
-                operacion = []
-                operacion.push(r)
-            }    
-        }
-        // operacion.push(op)
-        
-    }
-    operacion.forEach(element => {
-        console.log('|'+element+"|")
-    });
 }
 
 function cleanResult() {
